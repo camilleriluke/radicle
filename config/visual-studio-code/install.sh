b114b4903e1245ab4de215abac5279ce2579e0e4
Luke Camilleri <mail@camilleriluke.com>
2019-09-21 09:10:34 +0200

    feat: add the latest packages and casks used for d2d

--- config/visual-studio-code/install.sh
+++ config/visual-studio-code/install.sh
@@ -8,12 +8,13 @@ if [[ $? -eq 0 ]];then
     read -r -p "Do you want to install VSC extensions? [y|N] " configresponse
     if [[ $configresponse =~ ^(y|yes|Y) ]];then
         ok "Installing extensions please wait..."
-        code --install-extension DavidAnson.vscode-markdownlint
-        code --install-extension EditorConfig.editorconfig
-        code --install-extension JuanBlanco.solidity
-        code --install-extension RoscoP.activefileinstatusbar
+        code --install-extension RoscoP.ActiveFileInStatusBar
         code --install-extension TsumiNa.Seti-theme
-        code --install-extension abadi199.elm-format
+        code --install-extension alefragnani.Bookmarks
+        code --install-extension andys8.jest-snippets
+        code --install-extension chenxsan.vscode-standardjs
+        code --install-extension CoenraadS.bracket-pair-colorizer
+        code --install-extension DavidAnson.vscode-markdownlint
         code --install-extension dbaeumer.vscode-eslint
         code --install-extension dzannotti.vscode-babel-coloring
         code --install-extension eamodio.gitlens
@@ -22,17 +23,25 @@ if [[ $? -eq 0 ]];then
         code --install-extension fabiospampinato.vscode-todo-plus
         code --install-extension flowtype.flow-for-vscode
         code --install-extension gcazaciuc.vscode-flow-ide
+        code --install-extension JuanBlanco.solidity
+        code --install-extension mechatroner.rainbow-csv
+        code --install-extension ms-azuretools.vscode-docker
+        code --install-extension ms-vsliveshare.vsliveshare
         code --install-extension msjsdiag.debugger-for-chrome
+        code --install-extension numso.prettier-standard-vscode
+        code --install-extension pflannery.vscode-versionlens
         code --install-extension qinjia.seti-icons
         code --install-extension ryu1kn.partial-diff
         code --install-extension samverschueren.linter-xo
         code --install-extension sbrink.elm
+        code --install-extension sdras.night-owl
         code --install-extension shinnn.stylelint
         code --install-extension sleistner.vscode-fileutils
         code --install-extension stkb.rewrap
         code --install-extension streetsidesoftware.code-spell-checker
+        code --install-extension VisualStudioExptTeam.vscodeintellicode
+        code --install-extension wayou.vscode-todo-highlight
         code --install-extension wmaurer.change-case
-        code --install-extension zhuangtongfa.material-theme
 
         ok "Extensions for VSC have been installed. Please restart your VSC."
     else
